0bab90731dd8032d39250b7e40121f8ca8c54679
Elnaz mac <e.shamsedini@gmail.com>
2026-07-07 14:28:13 +0200

    initialiser base de donnée

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from app.api.routes.health import router as health_router
 from app.api.routes.receipts import router as receipts_router
+from app.db.database import engine, Base
+from app.models.receipt import Receipt
 
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Smart Receipt Analyzer",
