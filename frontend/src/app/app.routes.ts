ac1f751c4bceb98b216ec840027c2ba4f1949d87
Elnaz mac <e.shamsedini@gmail.com>
2026-07-08 14:36:49 +0200

    ajouter la page receipt

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { Receipts } from './pages/receipts/receipts';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component:Dashboard },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'receipts', component: Receipts },
 ];
---
+++ frontend/src/app/pages/receipts/receipts.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ReceiptListItem } from '../../core/models/receipt.model';
+import { ReceiptService } from '../../core/services/receipt.service';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-receipts',
+  imports: [DatePipe],
+  templateUrl: './receipts.html',
+  styleUrl: './receipts.css',
+})
+export class Receipts implements OnInit{
+  receipts: ReceiptListItem[] = [];
+  loading = false;
+  errorMessage = '';
+
+  constructor(
+    private receiptService: ReceiptService,
+  ){}
+
+  ngOnInit(): void {
+    this.loadReceipts();
+  }
+
+  loadReceipts(): void {
+    this.loading = true;
+    this.errorMessage = '';
+
+    this.receiptService.getReceipts().subscribe({
+      next: (data) => {
+        this.receipts = data;
+        this.loading = false;
+      },
+      error: () => {
+        this.errorMessage = 'Unable to load receipts.';
+        this.loading = false;
+      },
+    });
+  }
+
+  deleteReceipt(id: number):void{
+    this.receiptService.deleteReceipt(id).subscribe({
+      next: () => {
+        this.receipts = this.receipts.filter((receipt)=> receipt.id !== id);
+      },
+      error: () => {
+        this.errorMessage = 'Unable to delete the receipt.';
+      }
+    });
+  }
+}
